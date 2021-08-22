fb329de8a6746df2cf287042b43627d18e6ada19
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-08-22 09:46:54 -0700

    initial commit

---
+++ src/main/java/com/orbis/ssgascraper/SsgaScraperApplication.java
@@ -0,0 +1,13 @@
+package com.orbis.ssgascraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgaScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgaScraperApplication.class, args);
+	}
+
+}
