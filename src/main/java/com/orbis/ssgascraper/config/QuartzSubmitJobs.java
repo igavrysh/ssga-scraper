8bbf25f6006805021d63bb1e20497d92048f6e49
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-09-01 21:23:06 -0700

    change to every 5min start

--- src/main/java/com/orbis/ssgascraper/config/QuartzSubmitJobs.java
+++ src/main/java/com/orbis/ssgascraper/config/QuartzSubmitJobs.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 @Configuration
 public class QuartzSubmitJobs {
 
-  private static final String CRON_EVERY_THIRTY_MINUTES = "0 0/30 * ? * * *";
+  private static final String CRON_EVERY_N_MINUTES = "0 0/5 * ? * * *";
 
   // Runs at minute 0 past hour 6 and 18
   private static final String CRON_TWICE_PER_DAY = "0 0 10,15/12 * * ?";
@@ -23,6 +23,6 @@ public class QuartzSubmitJobs {
 
   @Bean(name = "scraperMainTrigger")
   public CronTriggerFactoryBean triggerScraperMain(@Qualifier("scraperMainJob123") JobDetail jobDetail) {
-    return QuartzConfig.createCronTrigger(jobDetail, CRON_EVERY_THIRTY_MINUTES, "Scrapper Main Trigger");
+    return QuartzConfig.createCronTrigger(jobDetail, CRON_EVERY_N_MINUTES, "Scrapper Main Trigger");
   }
 }
