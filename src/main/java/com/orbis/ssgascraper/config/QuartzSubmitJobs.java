eb703ccf47e75a484ccc7f791cc02664238940b3
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-09-05 10:08:36 -0700

    fix postman api json, update schedule file

--- src/main/java/com/orbis/ssgascraper/config/QuartzSubmitJobs.java
+++ src/main/java/com/orbis/ssgascraper/config/QuartzSubmitJobs.java
@@ -16,6 +16,9 @@ public class QuartzSubmitJobs {
   // Runs at minute 0 past hour 7 and 15
   private static final String CRON_TWICE_PER_DAY = "0 0 7,15/12 * * ?";
 
+  // Runs every 6 hours starting from 00:00
+  private static final String CRON_FOUR_TIMES_PER_DAY = "0 0 0,6,12,18/12 * * ?";
+
   @Bean(name = "scraperMainJob123")
   public JobDetailFactoryBean jobScraperMain() {
     return QuartzConfig.createJobDetail(ScraperMainJob.class, "Scrapper Main Job");
@@ -23,6 +26,6 @@ public class QuartzSubmitJobs {
 
   @Bean(name = "scraperMainTrigger")
   public CronTriggerFactoryBean triggerScraperMain(@Qualifier("scraperMainJob123") JobDetail jobDetail) {
-    return QuartzConfig.createCronTrigger(jobDetail, CRON_TWICE_PER_DAY, "Scrapper Main Trigger");
+    return QuartzConfig.createCronTrigger(jobDetail, CRON_FOUR_TIMES_PER_DAY, "Scrapper Main Trigger");
   }
 }
